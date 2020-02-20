fc33a53c4a8bb6a346ac902cf06abf003f2f16dd
ValtersHuns <valters.huns@grobinasskola.lv>
2020-02-20 19:27:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+@app.route('/about')
+def about():
+  return render_template('about.html')
+@app.route('/table')
+def table():
+  return render_template('table.html')
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
+
+app.run(host='0.0.0.0', port=8020)
