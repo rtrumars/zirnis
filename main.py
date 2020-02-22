7efaf74133cf1035d95e4c2e115aae106e9722e6
ValtersHuns <60145218+ValtersHuns@users.noreply.github.com>
2020-02-22 12:35:39 +0200

    Izmainas
    Nav vairs tabula, tagad ir lapa

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ app = Flask('app')
 @app.route('/')
 def home():
   return render_template('home.html')
-@app.route('/about')
-def about():
-  return render_template('about.html')
-@app.route('/table')
-def table():
-  return render_template('table.html')
+@app.route('/lapa')
+def lapa():
+  return render_template('lapa.html')
 @app.route('/contact')
 def contact():
   return render_template('contact.html')
